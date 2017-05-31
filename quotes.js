422c358e43692212f5c13ed2f33bcd0bfdcb4b43
Squibs <z.r.holman+github@gmail.com>
2017-05-30 20:54:57 -0600

    Fixed getJSON command.

--- quotes.js
+++ quotes.js
@@ -1,6 +1,5 @@
 $(".genQuote").on("click", function() {
-	$.getJSONP("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
+	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
 		$(".quotes").html(JSON.stringify(json));
 	});
 });
-
