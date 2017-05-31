1298af14820cbe487571f9a6e2cbebd5b02860af
Squibs <z.r.holman+github@gmail.com>
2017-05-30 20:52:25 -0600

    Changed API.

--- quotes.js
+++ quotes.js
@@ -1,5 +1,5 @@
 $(".genQuote").on("click", function() {
-	$.getJSON("http://api.forismatic.com/api/1.0/", function(json) {
+	$.getJSONP("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
 		$(".quotes").html(JSON.stringify(json));
 	});
 });
