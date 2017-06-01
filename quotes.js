f231325bfc83f02415d6b77fc281a708ed7b188e
Squibs <z.r.holman+github@gmail.com>
2017-06-01 02:09:53 -0600

    Pretty much completed this project.

--- quotes.js
+++
@@ -1,5 +0,0 @@
-$(".genQuote").on("click", function() {
-	$.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(json) {
-		$(".quotes").html(JSON.stringify(json));
-	});
-});
