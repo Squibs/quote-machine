0e6c35edc9c22c479def6dc2517827b68ffde410
Squibs <z.r.holman+github@gmail.com>
2017-08-19 21:24:25 -0600

    Hello react.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import './stylesheets/main.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
