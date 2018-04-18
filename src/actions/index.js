28ba8bf7d920ce10a6a37daffbb8fbe9f4592d00
Squibs <z.r.holman+github@gmail.com>
2018-04-18 14:35:46 -0600

    Added gh-pages for auto-deploy.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const FETCH_QUOTE = 'FETCH_QUOTE';
 export const CLEAR_QUOTE = 'CLEAR_QUOTE';
 
 export function fetchQuote() {
-  const request = axios.get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
+  const request = axios.get('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
 
   return {
     type: FETCH_QUOTE,
