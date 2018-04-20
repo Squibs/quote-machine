d666f52098acfde15769758be79c6da820195548
Squibs <z.r.holman+github@gmail.com>
2018-04-19 18:48:15 -0600

    Added timestamp to api request to avoid response being cached.

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,8 @@ export const FETCH_QUOTE = 'FETCH_QUOTE';
 export const CLEAR_QUOTE = 'CLEAR_QUOTE';
 
 export function fetchQuote() {
-  const request = axios.get('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1');
+  const ApiUrl = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+  const request = axios.get(`${ApiUrl}&timestamp=${new Date().getTime()}`); // timestamp to avoid response being cached
 
   return {
     type: FETCH_QUOTE,
