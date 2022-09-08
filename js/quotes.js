3977a7c3b0d0cb633ca48d797d646592be079a4e
Squibs <z.r.holman+github@gmail.com>
2022-09-08 03:58:48 -0600

    JS from my codepen.

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,7 @@
 onclickFlag = true;
 
+var post;
+
 function ajax_request() {
 	$(".content").hide();
 	$(".title").hide();
@@ -8,19 +10,26 @@ function ajax_request() {
 
 
 	setTimeout(function() {
-		$.ajax({
-			url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+		// $.ajax({
+		// 	url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+		// 	success: function(data) {
+    
+    // api url changed
+    $.ajax({
+			url: `https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand&per_page=1&timestamp=${new Date().getTime()}`,
+      type: 'GET',
+      async: true,
+      dataType: 'json',
 			success: function(data) {
-
-				post = data.shift();
+				post = data[0];
 				var contentLength = Math.floor(($(".note-card").width() * 470) * 0.0018);
 
 				if (post.content.length > contentLength) {
 					errorControl++;
 					ajax_request();
 				} else {
-					$(".title").html(post.title);
-					$(".content").html(post.content);
+					$(".title").html(post.title.rendered);
+					$(".content").html(post.content.rendered);
 
 					$(".loading").hide();
 					$(".content").show();
@@ -77,5 +86,5 @@ $(".quote-link").on("click", function(e) {
 
 
 $(".twitter-link").on("click", function() {
-	window.open("https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp,SquibsVids&text=" + encodeURIComponent('"' + $(post.content).text().trim() + '"' + " - " + post.title), '_blank');
+	window.open("https://twitter.com/intent/tweet?related=freeCodeCamp,SquibsVids&text=" + encodeURIComponent('"' + $(post.content.rendered).text().trim() + '"' + " - " + $(post.title.rendered).text().trim()), '_blank');
 });
